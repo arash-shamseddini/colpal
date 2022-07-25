d14bd8d9c1b2556ccc90c7e641362e4c377b41ad
arash-shamseddini <ashamseddini@edynamics.com>
2022-07-25 14:03:10 -0700

    Add edi_scale_col & edi_scale_fill functions & friends

--- R/pal_functions.R
+++ R/pal_functions.R
@@ -1,9 +1,13 @@
+########## edi_cols_vec ##########
+
 # Creating named vector for EDI colors (This can be modified in future)
 edi_cols_vec <- c(
   `Warm Blue` = "#007396",
   `Burnt Orange` = "#CF4520",
   `Dark Blue` = "#004F71"
 )
+
+########## edi_cols() ##########
 
 #' Creates EDI color HEX codes
 #'
@@ -26,14 +30,18 @@ edi_cols <- function(...) {
   edi_cols_vec[cols]
 }
 
+########## edi_palettes ##########
+
 # Creating EDI color palette (This can be modified in future)
 edi_palettes <- list(
-  `main`  = edi_cols("Warm Blue", "Burnt Orange", "Dark Blue"), # This list is open to potential changes in the future
+  `main`  = edi_cols("Warm Blue", "Burnt Orange", "Dark Blue"),
 
   `blue`  = edi_cols("Warm Blue", "Dark Blue"),
 
   `mixed` = edi_cols("Warm Blue", "Burnt Orange")
 )
+
+########## edi_pal() ##########
 
 #' Interpolates an EDI color palette
 #'
@@ -43,7 +51,8 @@ edi_palettes <- list(
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments to pass to colorRampPalette() function
 #'
-#' @return Character of hex codes of the given palette
+#' @return An interim function which takes the number of the levels within interpolation
+#' as the input.  Eventually this returns a character of hex codes of the given palette
 #'
 #' @examples
 edi_pal <- function(palette = "main", reverse = FALSE, ...) {
@@ -52,4 +61,76 @@ edi_pal <- function(palette = "main", reverse = FALSE, ...) {
   if (reverse) pal <- rev(pal)
 
   grDevices::colorRampPalette(pal, ...)
+}
+
+########## edi_scale_col() ##########
+
+#' Constructs color scale for EDI colors
+#'
+#' Function to create a color scale of the given palette
+#' @param palette Character name of palette in "edi_palettes"
+#' @param discrete Boolean indicating whether color aesthetic is discrete or continuous
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+#' @return A ggplot2 color scale based on the inputs
+#' @export
+#'
+#' @examples
+#' edi_scale_col("mixed", reverse = TRUE)
+edi_scale_col <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- edi_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    list(ggplot2::discrete_scale("colour", paste("EDI", palette, "scale", sep = " "), palette = pal, ...),
+         ggplot2::theme(axis.line = ggplot2::element_line(),   # Removing grid, color & borders
+               panel.grid.major = ggplot2::element_blank(),
+               panel.grid.minor = ggplot2::element_blank(),
+               panel.border = ggplot2::element_blank(),
+               panel.background = ggplot2::element_blank()))
+  } else {
+    list(ggplot2::scale_color_gradientn(colours = pal(256), ...),
+         ggplot2::theme(axis.line = ggplot2::element_line(),   # Removing grid, background color & borders
+               panel.grid.major = ggplot2::element_blank(),
+               panel.grid.minor = ggplot2::element_blank(),
+               panel.border = ggplot2::element_blank(),
+               panel.background = ggplot2::element_blank()))
+  }
+}
+
+########## edi_scale_fill() ##########
+
+#' Constructs fill scale for EDI colors
+#'
+#' Function to create a color scale of the given palette
+#' @param palette Character name of palette in "edi_palettes"
+#' @param discrete Boolean indicating whether color aesthetic is discrete or continuous
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+#' @return A ggplot2 fill scale based on the inputs
+#' @export
+#'
+#' @examples
+#' edi_scale_fill("mixed", discrete = FALSE, reverse = TRUE)
+edi_scale_fill <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- edi_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    list(ggplot2::discrete_scale("fill", paste("EDI", palette, "scale", sep = " "), palette = pal, ...),
+         ggplot2::theme(axis.line = ggplot2::element_line(),   # Removing grid, background color & borders
+               panel.grid.major = ggplot2::element_blank(),
+               panel.grid.minor = ggplot2::element_blank(),
+               panel.border = ggplot2::element_blank(),
+               panel.background = ggplot2::element_blank()))
+  } else {
+    list(ggplot2::scale_fill_gradientn(colours = pal(256), ...),
+         ggplot2::theme(axis.line = ggplot2::element_line(),   # Removing grid, color & borders
+               panel.grid.major = ggplot2::element_blank(),
+               panel.grid.minor = ggplot2::element_blank(),
+               panel.border = ggplot2::element_blank(),
+               panel.background = ggplot2::element_blank()))
+  }
 }
